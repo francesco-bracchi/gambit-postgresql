42c4e26089996ce48c2d44c8e57734ed9107777b
Your Name <frbracch@gmail.com>
2013-01-23 19:28:54 +0100

    added notification support

---
+++ src/flow/notifications#.scm
@@ -0,0 +1,12 @@
+(##namespace
+ ("postgresql/flow/notifications#"
+
+  make-notification
+  notification?
+  notification-pid
+  notification-channel
+  notification-payload
+  
+  notifications-table
+  recv-notification
+  ))
